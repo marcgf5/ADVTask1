8b6afba1a4b0d88065515411d0f6494962b45144
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-03-19 19:00:07 +0100

    Project 2 done

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ResetAnimation", 12.0f);
+    }
+
+    void ResetAnimation()
+    {
+        SceneManager.LoadScene("Project2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
