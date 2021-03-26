c6f97013196e910e5c34f7933180d06ba9065e89
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-03-26 19:18:30 +0100

    Task 5 done

---
+++ Assets/AnimationControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class AnimationControl : MonoBehaviour
+{
+
+    public Animator animator;
+    public string triggerParameter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger(triggerParameter);
+    }
+}
