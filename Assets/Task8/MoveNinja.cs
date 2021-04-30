afc3bc1d31ec139a113121fb15c8b891aae7213d
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-04-30 19:11:07 +0200

    Task 8 done

---
+++ Assets/Task8/MoveNinja.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveNinja : MonoBehaviour
+{
+
+    public Transform goal;
+    NavMeshAgent agent;
+    Animator animatorPersonaje;
+    public Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animatorPersonaje = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        animatorPersonaje.SetFloat("vertical", transform.InverseTransformDirection(agent.velocity).z);
+        animatorPersonaje.SetFloat("horizontal", transform.InverseTransformDirection(agent.velocity).x);
+    }
+    
+}
