6bf13c72b77550e842080c06de9fe3882b407e25
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-05-21 01:00:54 +0200

    Finishing cinematic done

---
+++ Assets/BoatActivator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BoatActivator : MonoBehaviour
+{
+
+    [SerializeField] private PlayableDirector timeline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("BoatActivator"))
+            timeline.Play();
+    }
+}
--- Assets/Scripts/ResetScene.cs
+++ Assets/Scripts/ResetScene.cs
@@ -5,20 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class ResetScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        Invoke("ResetAnimation", 12.0f);
-    }
-
-    void ResetAnimation()
-    {
-        SceneManager.LoadScene("Project2");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Project");
     }
 }
+
