30e7942f18fce734e785888888527f8b1c6efdb3
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-05-21 10:37:50 +0200

    Project finished

--- Assets/BoatActivator.cs
+++ Assets/BoatActivator.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class BoatActivator : MonoBehaviour
 {
 
     [SerializeField] private PlayableDirector timeline;
+    [SerializeField] private Text appleNumberText;
+    [SerializeField] private AudioSource appleSound;
+
+    private int appleNumber = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +28,19 @@ public class BoatActivator : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.tag.Equals("BoatActivator"))
-            timeline.Play();
+        if (other.tag.Equals("BoatActivator"))
+        {
+            if (appleNumber >= 5)
+            {
+                timeline.Play();
+            }
+        } else if (other.tag.Equals("Apple"))
+        {
+            appleNumber += 1;
+            GameObject.Destroy(other.gameObject);
+            appleNumberText.text = appleNumber.ToString();
+            appleSound.Play();
+        }
+            
     }
 }
